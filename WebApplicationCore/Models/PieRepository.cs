182cce936328a99c820818fd659c2799c07c2e05
venkatesh.valleri <venki.valleri@hotmail.com>
2019-11-25 16:20:23 +0000

    upto 5th module - Gill Cleeren

---
+++ WebApplicationCore/Models/PieRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationCore.Models
+{
+    public class PieRepository : IPieRepository
+    {
+        private readonly AppDbContext _appDbcontext;
+
+        public PieRepository(AppDbContext appDbcontext)
+        {
+            _appDbcontext = appDbcontext;
+        }
+        public IEnumerable<Pie> AllPies
+        {
+            get
+            {
+                return _appDbcontext.Pies.Include(c => c.Category);
+            }
+        } 
+
+        public IEnumerable<Pie> PiesOfTheWeek
+        {
+            get
+            {
+                return _appDbcontext.Pies.Include(c => c.Category).Where(p => p.IsPieOfTheWeek);
+            }
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+            return _appDbcontext.Pies.FirstOrDefault(p => p.PieId==pieId);
+        }
+    }
+}
